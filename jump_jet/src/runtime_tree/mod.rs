8f0aa6a6ac97426427e68f82f44d67167b398aae
James Morrison <jamesmorrison3@live.co.uk>
2018-01-27 17:49:52 +0000

    Added empty interpreter for all ops

--- jump_jet/src/runtime_tree/mod.rs
+++ jump_jet/src/runtime_tree/mod.rs
@@ -159,8 +159,32 @@ impl RuntimeModule {
                         Operation::Block(ref b) => {
                             // TODO maybe do stuff recursively here, because I'm lazy af
                         },
-                        Operation::GetLocal(idx) => {
-                            stack.push(local_space[idx].clone());
+                        Operation::Loop(ref b) => {
+
+                        },
+                        Operation::If(ref b) => {
+
+                        },
+                        Operation::Else => {
+
+                        },
+                        Operation::End => {
+                            break
+                        },
+                        Operation::Branch(i) => {
+
+                        },
+                        Operation::BranchIf(i) => {
+
+                        },
+                        Operation::BranchTable(ref bt) => {
+
+                        },
+                        Operation::Return => {
+
+                        },
+                        Operation::Call(i) => {
+
                         },
                         Operation::CallIndirect(idx, _) => {
                             let &TypeDefinition::Func(ref signature) = &(module.types)[idx];
@@ -208,12 +232,97 @@ impl RuntimeModule {
                                 panic!("function not found or not indexed by i32");
                             }
                         },
+                        Operation::Drop => {},
+                        Operation::Select => {},
+                        Operation::GetLocal(idx) => {
+                            stack.push(local_space[idx].clone());
+                        },
+                        Operation::SetLocal(idx) => {
+
+                        },
+                        Operation::TeeLocal(idx) => {
+
+                        },
+                        Operation::GetGlobal(idx) => {
+
+                        },
+                        Operation::SetGlobal(idx) => {
+
+                        },
+                        Operation::I32Load(ref mem) => {},
+                        Operation::I64Load(ref mem) => {},
+                        Operation::F32Load(ref mem) => {},
+                        Operation::F64Load(ref mem) => {},
+                        Operation::I32Load8S(ref mem) => {},
+                        Operation::I32Load8U(ref mem) => {},
+                        Operation::I32Load16S(ref mem) => {},
+                        Operation::I32Load16U(ref mem) => {},
+                        Operation::I64Load8S(ref mem) => {},
+                        Operation::I64Load8U(ref mem) => {},
+                        Operation::I64Load16S(ref mem) => {},
+                        Operation::I64Load16U(ref mem) => {},
+                        Operation::I64Load32S(ref mem) => {},
+                        Operation::I64Load32U(ref mem) => {},
+                        Operation::I32Store(ref mem) => {},
+                        Operation::I64Store(ref mem) => {},
+                        Operation::F32Store(ref mem) => {},
+                        Operation::F64Store(ref mem) => {},
+                        Operation::I32Store8(ref mem) => {},
+                        Operation::I32Store16(ref mem) => {},
+                        Operation::I64Store8(ref mem) => {},
+                        Operation::I64Store16(ref mem) => {},
+                        Operation::I64Store32(ref mem) => {},
+                        Operation::CurrentMemory(_) => {},
+                        Operation::GrowMemory(_) => {},
                         Operation::I32Const(value) => {
                             stack.push(ValueTypeProvider::I32(value));
                         },
-                        Operation::End => {
-                            break
+                        Operation::I64Const(value) => {
+                            stack.push(ValueTypeProvider::I64(value));
                         },
+                        Operation::F32Const(value) => {
+                            stack.push(ValueTypeProvider::F32(value));
+                        },
+                        Operation::F64Const(value) => {
+                            stack.push(ValueTypeProvider::F64(value));
+                        },
+                        Operation::I32Eqz => {},
+                        Operation::I32Eq => {},
+                        Operation::I32Ne => {},
+                        Operation::I32LtS => {},
+                        Operation::I32LtU => {},
+                        Operation::I32GtS => {},
+                        Operation::I32GtU => {},
+                        Operation::I32LeS => {},
+                        Operation::I32LeU => {},
+                        Operation::I32GeS => {},
+                        Operation::I32GeU => {},
+                        Operation::I64Eqz => {},
+                        Operation::I64Eq => {},
+                        Operation::I64Ne => {},
+                        Operation::I64LtS => {},
+                        Operation::I64LtU => {},
+                        Operation::I64GtS => {},
+                        Operation::I64GtU => {},
+                        Operation::I64LeS => {},
+                        Operation::I64LeU => {},
+                        Operation::I64GeS => {},
+                        Operation::I64GeU => {},
+                        Operation::F32Eq => {},
+                        Operation::F32Ne => {},
+                        Operation::F32Lt => {},
+                        Operation::F32Gt => {},
+                        Operation::F32Le => {},
+                        Operation::F32Ge => {},
+                        Operation::F64Eq => {},
+                        Operation::F64Ne => {},
+                        Operation::F64Lt => {},
+                        Operation::F64Gt => {},
+                        Operation::F64Le => {},
+                        Operation::F64Ge => {},
+                        Operation::I32Clz => {},
+                        Operation::I32Ctz => {},
+                        Operation::I32Popcnt => {},
                         Operation::I32Add => {
                             if let Some(ValueTypeProvider::I32(a)) = stack.pop() {
                                 if let Some(ValueTypeProvider::I32(b)) = stack.pop() {
@@ -225,7 +334,91 @@ impl RuntimeModule {
                                 panic!("first operand must be i32");
                             }
                         }
-                        _ => panic!("not supported yet")
+                        Operation::I32Sub => {},
+                        Operation::I32Mul => {},
+                        Operation::I32DivS => {},
+                        Operation::I32DivU => {},
+                        Operation::I32RemS => {},
+                        Operation::I32RemU => {},
+                        Operation::I32And => {},
+                        Operation::I32Or => {},
+                        Operation::I32Xor => {},
+                        Operation::I32Shl => {},
+                        Operation::I32ShrS => {},
+                        Operation::I32ShrU => {},
+                        Operation::I32Rotl => {},
+                        Operation::I32Rotr => {},
+                        Operation::I64Clz => {},
+                        Operation::I64Ctz => {},
+                        Operation::I64Popcnt => {},
+                        Operation::I64Add => {},
+                        Operation::I64Sub => {},
+                        Operation::I64Mul => {},
+                        Operation::I64DivS => {},
+                        Operation::I64DivU => {},
+                        Operation::I64RemS => {},
+                        Operation::I64RemU => {},
+                        Operation::I64And => {},
+                        Operation::I64Or => {},
+                        Operation::I64Xor => {},
+                        Operation::I64Shl => {},
+                        Operation::I64ShrS => {},
+                        Operation::I64ShrU => {},
+                        Operation::I64Rotl => {},
+                        Operation::I64Rotr => {},
+                        Operation::F32Abs => {},
+                        Operation::F32Neg => {},
+                        Operation::F32Ceil => {},
+                        Operation::F32Floor => {},
+                        Operation::F32Trunc => {},
+                        Operation::F32Nearest => {},
+                        Operation::F32Sqrt => {},
+                        Operation::F32Add => {},
+                        Operation::F32Sub => {},
+                        Operation::F32Mul => {},
+                        Operation::F32Div => {},
+                        Operation::F32Min => {},
+                        Operation::F32Max => {},
+                        Operation::F32Copysign => {},
+                        Operation::F64Abs => {},
+                        Operation::F64Neg => {},
+                        Operation::F64Ceil => {},
+                        Operation::F64Floor => {},
+                        Operation::F64Trunc => {},
+                        Operation::F64Nearest => {},
+                        Operation::F64Sqrt => {},
+                        Operation::F64Add => {},
+                        Operation::F64Sub => {},
+                        Operation::F64Mul => {},
+                        Operation::F64Div => {},
+                        Operation::F64Min => {},
+                        Operation::F64Max => {},
+                        Operation::F64Copysign => {},
+                        Operation::I32WrapI64 => {},
+                        Operation::I32TruncSF32 => {},
+                        Operation::I32TruncUF32 => {},
+                        Operation::I32TruncSF64 => {},
+                        Operation::I32TruncUF64 => {},
+                        Operation::I64ExtendSI32 => {},
+                        Operation::I64ExtendUI32 => {},
+                        Operation::I64TruncSF32 => {},
+                        Operation::I64TruncUF32 => {},
+                        Operation::I64TruncSF64 => {},
+                        Operation::I64TruncUF64 => {},
+                        Operation::F32ConvertSI32 => {},
+                        Operation::F32ConvertUI32 => {},
+                        Operation::F32ConvertSI64 => {},
+                        Operation::F32ConvertUI64 => {},
+                        Operation::F32DemoteF64 => {},
+                        Operation::F64ConvertSI32 => {},
+                        Operation::F64ConvertUI32 => {},
+                        Operation::F64ConvertSI64 => {},
+                        Operation::F64ConvertUI64 => {},
+                        Operation::F64PromoteF32 => {},
+                        Operation::I32ReinterpretF32 => {},
+                        Operation::I64ReinterpretF64 => {},
+                        Operation::F32ReinterpretI32 => {},
+                        Operation::F64ReinterpretI64 => {}
                     }
                 }
                 let mut results = vec![];
